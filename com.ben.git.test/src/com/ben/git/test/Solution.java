7d9d16eab1d6bb46aaa3249b8d6e31a1af5b6f9a
benasherding <Benja@NEW-VNEV2OV5FVB>
2020-05-21 09:49:39 +0800

    1

--- com.ben.git.test/src/com/ben/git/test/Solution.java
+++ com.ben.git.test/src/com/ben/git/test/Solution.java
@@ -1,5 +1,90 @@
 package com.ben.git.test;
 
+
+
+
+
+
+class Animal {
+	void walk() {
+		System.out.println("I am walking");
+	}
+	
+}
+
+class Bird extends Animal {
+	
+
+	void fly() {
+		System.out.println("I am flying");
+	}
+	
+	void sing() {
+		System.out.println("I am Singing");
+	}
+}
+
+
+class Duck extends Bird {
+	void sound() {
+		System.out.println("Quack Quack");
+		System.out.println("I can swim~");
+	}
+	
+	
+	
+}
+class Chicken extends Bird{
+	void sound() {
+		System.out.println("Cluck cluck");
+		System.out.println("I cannot fly");
+	}
+	
+	
+	
+}
+
+
+
+
+//reusing codes fly and cannot fly, swim cannot swim
+//reusing codes cock-adoodle.. meow, woof woof,
+//
+
+
+
+
+
+
+/*
+ * class Rooster extends Chicken{
+ * 
+ * void sound() { System.out.println("Cock-a-doodle-doo");
+ * System.out.println("I am a male Chicken"); }
+ * 
+ * 
+ * 
+ * 
+ * }
+ * 
+ */
+
+
+
 public class Solution {
+	public static void main(String[] args) {
+		
+		
+		
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+		Duck duck = new Duck();
+		Chicken chicken = new Chicken();
+		duck.sound();
+		chicken.sound();
+		
+	}
 
 }
